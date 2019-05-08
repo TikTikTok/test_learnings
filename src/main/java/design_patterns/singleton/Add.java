78ab8c8ba50d57a8a917e8411516872df2494792
TikTikTok <chandan.singh116@gmail.com>
2019-05-08 10:26:22 +0530

    commit 1

--- src/main/java/design_patterns/singleton/Add.java
+++ src/main/java/design_patterns/singleton/Add.java
@@ -3,5 +3,6 @@ package design_patterns.singleton;
 public class Add {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println(" Git checkout comment 1");
     }
 }
