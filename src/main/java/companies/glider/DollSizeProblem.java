6ec18fb869ba4be292be6d12da3fe1214ae54e96
TikTikTok <chandan.singh116@gmail.com>
2019-06-17 15:16:26 +0530

    added max no of dolls solution

--- src/main/java/companies/glider/DollSizeProblem.java
+++ src/main/java/companies/glider/DollSizeProblem.java
@@ -2,25 +2,27 @@ package companies.glider;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Set;
 
 public class DollSizeProblem {
 
     public static void main(String[] args) {
 
-        System.out.println(" start ");
-        int ar[] = {5, 2, 3, 2, 3};
-        Arrays.sort(ar);
+        int ar[] = {5, 2, 3, 3, 3, 3, 3, 2, 3};
+        //Arrays.sort(ar);
         HashMap<Integer, Integer> map = new HashMap<>();
+        int max = 0;
 
         for (int i = 0; i < ar.length; i++) {
             if (map.containsKey(ar[i])) {
-                map.put(ar[i], map.get(ar[i])+1);
+                if (max < map.get(ar[i]) + 1)
+                    max = map.get(ar[i]) + 1;
+                map.put(ar[i], max);
             } else {
                 map.put(ar[i], 1);
             }
         }
-
-
         map.forEach((key, value) -> System.out.println(" Element: " + key + " Count: " + value));
+        System.out.println(" Max no of dolls " + max);
     }
 }
