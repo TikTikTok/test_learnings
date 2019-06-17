dd7fdc5fe3474d2edb6c934bf4a271b7dbf74fdd
TikTikTok <chandan.singh116@gmail.com>
2019-06-17 21:30:57 +0530

    book shelf problem

--- src/main/java/companies/eka_software/BookShelfProblem.java
+++ src/main/java/companies/eka_software/BookShelfProblem.java
@@ -4,15 +4,14 @@ import java.util.Scanner;
 
 public class BookShelfProblem {
 
-    static int shelf[] = new int[10];
+    static int shelf[] = {3, 5, 1, 7};
     static int shelfNo[] = new int[3];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(" Do you want to add elemnent : (Yes/No) ? ");
         String addOrRemove = sc.next();
-        if (("Y".equals(addOrRemove.toUpperCase())) || ("y".equals(addOrRemove.toLowerCase())))
-        {
+       /* if (("Y".equals(addOrRemove.toUpperCase())) || ("y".equals(addOrRemove.toLowerCase()))) {
             System.out.println(" Enter the shelf no: ");
             for (int i = 0; i < 3; i++) {
                 shelfNo[i] = sc.nextInt();
@@ -22,14 +21,42 @@ public class BookShelfProblem {
                 pushBookToShelf(shelfNo, sc.nextInt());
             }
         }
+*/
+        if (("N".equals(addOrRemove.toUpperCase())) || ("n".equals(addOrRemove.toLowerCase()))) {
 
-        if (("N".equals(addOrRemove.toUpperCase())) || ("n".equals(addOrRemove.toLowerCase())))
-        {
-
+            System.out.println(" Enter the shelf no: ");
+            for (int i = 0; i < 3; i++) {
+                int data = sc.nextInt();
+                popBooks(data);
+            }
         }
 
         for (int i = 0; i < shelf.length; i++) {
             System.out.println(" Pring :" + shelf[i]);
+        }
+    }
+
+    public static void popBooks(int data) {
+        for (int i = 0; i < shelf.length; i++) {
+            if (shelf[i] == data) {
+                shelf[i] = 0;
+                for (int j = i; j < shelf.length-i ; j++) {
+                    shelf[j]= shelf[j+1];
+                }
+               shelf[shelf.length-1]=0;
+            } /*else {
+                for (int i = 0; i < shelf.length; i++) {
+                    shelf[shelfNumber[i]] = data;
+                    for (int j = 0; j < shelf.length; j++) {
+                        if (shelf[j] == 0) {
+                            shelf[j] = shelf[shelfNumber[i]];
+                            shelf[shelfNumber[i]] = 0;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }*/
         }
     }
 
@@ -51,6 +78,9 @@ public class BookShelfProblem {
             }
         }
     }
+
+
+
 }
 
 
